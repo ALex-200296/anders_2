120f2dd8ff0cc6d01d6df07ab6584f198fac282e
Александр Ли <li.aa2012@yandex.ru>
2021-09-02 12:57:59 +0300

    section empty

--- src/components/EmptySection/EmptySection.tsx
+++ src/components/EmptySection/EmptySection.tsx
@@ -1,10 +1,58 @@
 import React from "react";
 import styles from "./emptySection.module.scss";
+import image from "../../assets/380373.jpg";
+
+interface IActors {
+  name: string;
+}
+
+const actors: IActors[] = [
+  {
+    name: "Том Холланд",
+  },
+  {
+    name: "Альфред Молина",
+  },
+  {
+    name: "Зендея",
+  },
+  {
+    name: "Мариса Томей",
+  },
+  {
+    name: "Джейкоб Баталон",
+  },
+  {
+    name: "Бенедикт КамберБэтч",
+  },
+  {
+    name: "Джонатан Кимбл Симмонс",
+  },
+  {
+    name: "Джон Фавро",
+  },
+];
 
 const EmptySection: React.FC = () => {
   return (
     <div className={styles.empty}>
-      <h1 className={styles.about}>EmptyFilm</h1>
+      <img src={image} alt="человек-паук" className={styles.img} />
+      <div className={styles.wrap}>
+        <p className={styles.descr}>
+          <h2>Описание</h2>
+          Впервые в киноистории Человека-паука наш дружелюбный герой разоблачен.
+          Теперь супергеройские подвиги стали неотделимы от его обычной жизни.
+          Когда он просит помощи у Доктора Стрэнджа, ситуация только накаляется.
+          И Питер Паркер начинает понимать, что такое быть Человеком-пауком на
+          самом деле.
+        </p>
+        <h2>Актеры</h2>
+        <ul className={styles.list}>
+          {actors.map((item: IActors, idx: number) => (
+            <li key={idx}>{item.name}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,10 +1,15 @@
 import React from "react";
+import Loading from "../Loading/Loading";
 import styles from "./search.module.scss";
 
 const Search: React.FC = () => {
   return (
     <div className={styles.search}>
-      <h1>Section Search</h1>
+      <form className={styles.form}>
+        <Loading />
+        <input type="text" className={styles.input} />
+        <button className={styles.btn}>поиск</button>
+      </form>
     </div>
   );
 };
